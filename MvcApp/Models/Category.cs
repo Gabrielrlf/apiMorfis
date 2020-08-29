e4f87d503afdf0a611402124dc85186365400638
Gabriel <biellimaet@gmail.com>
2020-08-29 16:34:39 -0300

    Adicionar arquivos de projeto.

---
+++ MvcApp/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using MvcApp.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApp.Models
+{
+    public class Category
+    {
+        public  int Id { get; set; }
+        [Display(Name = "Nome")]
+
+        [Required(ErrorMessage = "Esse campo é obrigatório!")]
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
